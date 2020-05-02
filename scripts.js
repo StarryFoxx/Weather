9e4377cfa239e1e31d27c180412b0c9adc5b0880
Joshua Todd <joshtodd2319@yahoo.com>
2020-05-02 14:19:39 -0400

    j

---
+++ scripts.js
@@ -0,0 +1,8 @@
+
+let sum2 = (a,b) => a - b
+
+document.write(sum2(8,2));
+console.log("hello");
+
+console.log("http://api.openweathermap.org/data/2.5/forecast?id=524901&APPID={47a563015f3befe22084a3ec7356bbbf
+}");
