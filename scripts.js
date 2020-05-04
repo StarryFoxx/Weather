e1e5ad51f0bcb496b99ad8aa7031142a41ae9af3
Joshua Todd <joshtodd2319@yahoo.com>
2020-05-04 13:05:08 -0400

    fixed

--- scripts.js
+++ scripts.js
@@ -4,7 +4,10 @@ function getWeather(){
     console.log(response);
     console.log(removeResponse);
 
-    $(".weatherResponse").empty();
+    $(".weatherCity").empty();
+    $(".weatherDescription").empty();
+    $(".weatherType").empty();
+    $(".weatherTemp").empty();
 
     var cityName = $('#cityName').val();
     var apiCall = "http://api.openweathermap.org/data/2.5/weather?q=" + cityName + "&appid=3402441ac6d47ba974ee3af8940a579a";
@@ -13,32 +16,102 @@ function getWeather(){
 function weatherCallback(weatherData) {
   var cityName = weatherData.name;
   var country = weatherData.sys.country;
-  var temp = Math.round(weatherData.main.temp);
-  var description = weatherData.weather[0].description;
+  var description = weatherData.weather[0].description.toUpperCase();
 
 
-  $('.weatherResponse').append(cityName + ", " + country + " has " + description + " today! It's " + temp + "!");
+  var k = Math.round(weatherData.main.temp);
+  var f = Math.round((9 / 5) * (k - 273) + 32);
+  var c = Math.round(( 5 / 9) * (f - 32));
 
-  var clear = description.includes("clear"); //clear/sunny
-  var cloud = description.includes("cloud"); //cloudy
-  var rain = description.includes("rain"); //rainy
-  var snow = description.includes("snow"); //snow
-  var storm = description.includes("storm"); //storm
+  $('.weatherCity').append(cityName + ", " + country);
+  $('.weatherDescription').append(description);
+  $('.weatherIcon').append();
 
-  if (clear == true) {
-    console.log("sunny");
-  } else if (cloud == true) {
-    console.log("cloudy");
-  } else if (rain == true) {
-    console.log("raining");
-  } else if (snow == true) {
-    console.log("snowing");
-  } else if (storm == true) {
-    console.log("storming");
-  } else {
-    console.log("error")
+  //Capitalize first letter in each word
+
+  $('.weatherType').append(description);
+
+  $('.weatherTemp').append(f + "°" + "F"); //calls farhenheit default
+
+//if else here
+
+
+} //end weatherCallback
+
+} //end getWeather
+
+//start changeData
+
+  //for now made included strings capitalized due to .toUpperCase() on description variable - around line 19
+function changeData(){
+
+
+  setTimeout(changeData, .1); //timeout allows innerHTML to populate before this function pulls data from it!
+  var description = document.querySelector('.weatherType').innerHTML;
+
+  var clear = description.includes("CLEAR"); //clear/sunny
+  var cloud = description.includes("CLOUD"); //cloudy
+  var rain = description.includes("RAIN"); //rainy
+  var snow = description.includes("SNOW"); //snow
+  var storm = description.includes("STORM"); //storm
+
+  //  $('.weatherIcon').removeClass();
+
+  //make changes based on weather
+  if (clear == true) { //clear/sunny
+    $('i').removeClass();
+    $('i').addClass('fas fa-sun');
+    return;
+  } else if (cloud == true) { //cloudy
+    $('i').removeClass();
+    $('i').addClass('fas fa-cloud');
+    return;
+  } else if (rain == true) { //raining
+    $('i').removeClass();
+    $('i').addClass('fas fa-cloud-showers-heavy');
+    return;
+  } else if (snow == true) { //snowing
+    $('i').removeClass();
+    $('i').addClass('fas fa-snowflake');
+    return;
+  } else if (storm == true) { //storming
+    $('i  ').removeClass();
+    $('i').addClass('fas fa-bolt');
+    return;
+  } else { //error
+    return;
   }
 
 
-}
-}
+} //end changeData
+
+function  changeTemp(){
+  setTimeout(changeTemp, .5);
+  var temp = document.querySelector('.weatherTemp').innerHTML;
+
+  if (temp.includes("F")){ //change F to C
+    var f = temp;
+    var c = Math.round(( 5 / 9) * (f - 32));
+    return;
+  } else if (temp.includes("C")) { //change C to K
+    var c = temp;
+    $('.weatherTemp').empty();
+    $('.weatherTemp').append(k + "°" + "K");
+  } else if (temp.includes("K")) { //change K to F
+    var k = temp;
+    $('.weatherTemp').empty();
+    $('.weatherTemp').append(f + "°" + "F");
+  } else
+
+  return;
+} //end changeTemp
+
+
+
+
+
+
+
+
+
+//run changeData after getWeather
